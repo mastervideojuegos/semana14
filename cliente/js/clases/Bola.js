c0568d72e559cd4dd34fad170f3fafdea2b2099c
jferab <jferab@gmail.com>
2019-03-16 10:38:43 +0100

    test atom

--- cliente/js/clases/Bola.js
+++ cliente/js/clases/Bola.js
@@ -3,38 +3,38 @@ function Bola()
     this.tamanio    = 16;
     this.posX       = Math.random()*512;
     this.posY       = Math.random()*512 ;
-	
+
     this.velocidad  = 1;
     this.avanceX    = 5;
     this.avanceY    = 5;
 
-this.pelotaRandom = function(){
-    this.posX       = Math.random()*512;
-    this.posY       = Math.random()*512 ;
+  this.pelotaRandom = function(){
+      this.posX       = Math.random()*512;
+      this.posY       = Math.random()*512 ;
 
-}
+  }
     this.colicionPared = function(px,py)
     {
         if(px <=-45 || px >=1300)
         {
             this.pelotaRandom();
-			//this.avanceX *=-1;        
+			//this.avanceX *=-1;
         }
         if(py <=65 || py >=720)
         {
-            this.avanceY *=-1;        
+            this.avanceY *=-1;
         }
     }
-    
+
     this.colicionPersonajes = function(px,py){
-		if( px >= personaje.posX &&		
+		if( px >= personaje.posX &&
 			py >=(personaje.posY -personaje.largo) &&
 			py <=(personaje.posY +personaje.largo)
 			){
 			console.log("colicion")
-			this.avanceX *=-1;    
+			this.avanceX *=-1;
 		}
-		
+
 	}
 
     this.moverse = function()
@@ -44,13 +44,13 @@ this.pelotaRandom = function(){
 
         this.colicionPared(tmpX,tmpY);
 		this.colicionPersonajes(tmpX,tmpY);
-        
+
         this.posX += this.avanceX * this.velocidad;
         this.posY += this.avanceY * this.velocidad;
     }
-    
+
     this.Dibujar = function(ctx,camara)
     {
         ctx.drawImage(bolaActual, this.posX+camara.posX, this.posY+camara.posY, this.tamanio, this.tamanio);
-    }   
+    }
 }
