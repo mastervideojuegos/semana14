fbad287c4887901e5d314150ed49b165fa39f7a9
jferab <jferab@gmail.com>
2019-03-16 10:41:30 +0100

    test

--- cliente/js/clases/Personaje.js
+++ cliente/js/clases/Personaje.js
@@ -3,22 +3,21 @@ function Personaje()
     this.largo      = 32;
     this.posX       = 600;
     this.posY       = Math.random()*612+this.largo;
-    
     this.vel = 10;
     this.direccion = 1;
-    
+
     this.colicionPared = function()
     {
         if(this.posY + this.largo/2 > 725)
         {
             this.posY = 725 - this.largo/2;
-        } 
+        }
         else if(this.posY - this.largo/2 <65)
         {
             this.posY = 65 + this.largo/2;
         }
     }
-    
+
     this.moverse = function()
     {
         if(!bCongelaJugador)
@@ -27,9 +26,9 @@ function Personaje()
             this.colicionPared();
         }
     }
-    
+
     this.Dibujar = function(ctx,camara)
     {
         ctx.drawImage(personajeActual, this.posX+camara.posX, this.posY+camara.posY-this.largo*0.5, this.largo*0.5, this.largo);
-    } 
+    }
 }
