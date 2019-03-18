b353d481fa5fd785f1cabe809b4cadf01a5dce01
jferab <jferab@gmail.com>
2019-03-18 22:56:34 +0100

    20190318 v2

--- cliente/js/clases/Bola.js
+++ cliente/js/clases/Bola.js
@@ -1,12 +1,16 @@
 function Bola()
 {
-  this.tamanio    = 16;
+  this.tamanio    = 8;
+  this.tamanioOriginal = this.tamanio;
+
   this.posX       = Math.random()*512;
   this.posY       = Math.random()*512 ;
 
   this.velocidad  = 1;
   this.avanceX    = 5;
   this.avanceY    = 5;
+
+  //this.poderes = new Poderes();
 
   this.pelotaRandom = function(){
     this.posX       = Math.random()*512;
@@ -16,6 +20,8 @@ function Bola()
   this.colicionPared = function(px,py){
     if(px <=-45 || px >=1300){
       this.pelotaRandom();
+
+      //Punto!!!!!
 		}
     if(py <=65 || py >=720){
       this.avanceY *=-1;
@@ -45,6 +51,8 @@ this.moverse = function(){
 
         this.posX += this.avanceX * this.velocidad;
         this.posY += this.avanceY * this.velocidad;
+
+        //this.tamanio *= this.poderes(superbola)
     }
 
     this.dibujar = function(ctx,camara)
