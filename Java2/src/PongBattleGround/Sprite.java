b2a765acdb7b14e490a14bc9efbed44728396db1
stormk90 <44515776+stormk90@users.noreply.github.com>
2019-03-18 21:27:54 +0100

    test java2

---
+++ Java2/src/PongBattleGround/Sprite.java
@@ -0,0 +1,51 @@
+//Coge las Medidas del Sprite Para Colisiones
+
+package PongBattleGround;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+
+public class Sprite {
+
+    protected int posX;
+    protected int posY;
+    protected int width;
+    protected int heigth;
+    protected Image image;
+
+
+    public void setX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getX() {
+        return posX;
+    }
+
+    public void setY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getY() {
+        return posY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return heigth;
+    }
+
+    Image getImage()
+    {
+      return image;
+    }
+
+    Rectangle getRect()
+    {
+      return new Rectangle(posX, posY, 
+          image.getWidth(null), image.getHeight(null));
+    }
+}
